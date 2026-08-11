33f2734d7090dfd5306c0cd3edc3af521539f0de
Mohammed Ennaceur <ennaceur.drifa@hotmail.com>
2026-08-11 19:24:13 +0100

    Updated spdr.c
    yeah

--- spdr.c
+++ spdr.c
@@ -1,12 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/wait.h>
-#include <unistd.h>
-#include "spdr-func.c"
+
+#include "spdr-func.c" //boo hoo! i didn't use a .h file! it broke ):
 //sudo ./spdr install textmite
 int main(int argc, char *argv[]){ //very small int main woo hoo!
-    check_for_error(argv[1]); //we check whether argv[1] exists because update in the future will just auto update without argv[2].
+    check_for_error(argv[1]); //we check whether argv[1] is update so sudo spdr update works
     if (geteuid() != 0) { //checks whether it has root access
         printf("i need root in order to function so please retype with sudo at the front\n");
         printf("eg; sudo spdr install neofetch\n"); //you know i could technically combine this into check error but i feel like thats overdoing it
@@ -20,11 +16,11 @@ int main(int argc, char *argv[]){ //very small int main woo hoo!
         check_for_error(argv[2]);
         delete_file(argv[2]);
     } else if (strcmp(argv[1], "update") == 0) {
-        if (argc > 1){ //check whether it wants everything or not
+        if (argv[2] != NULL){ //check whether it wants everything or not
             check_for_error(argv[2]);
             update_one_thing(argv[2]);
         }else{
-            printf("adding soon\n"); //soon... yeap.. soon..
+            scan_dir(); //holy hell this is freakin sweet lois! it updates!
         }
     } else {
         printf("Invalid command/argument\n"); //checks whether the argument is a phony! a big, fat phony!
